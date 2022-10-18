7efedd71694f32b300c490ff61a4c99c8fe4d10e
wherzberg <70026035+wherzberg@users.noreply.github.com>
2022-10-18 12:21:17 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='GN4IP',
+    version='1.0.0',
+    description='Graph Networks for Inverse Problems',
+    url='http://github.com/wherzberg/GN4IP',
+    author='William Herzberg',
+    author_email='william.herzberg@marquette.edu',
+    packages=[
+        'GN4IP'
+    ]
+)
