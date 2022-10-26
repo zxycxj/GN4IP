4de9465bc2c474916702a0fc85b390556c5c0730
wherzberg <70026035+wherzberg@users.noreply.github.com>
2022-10-26 13:35:06 -0500

    Deleted out of data files

--- GN4IP/old/experiment/__init__.py
+++
@@ -1,6 +0,0 @@
-# GN4IP/models __init__ file
-
-from .experiment import experiment
-from .trainingParameters import trainingParameters
-from .trainingResults import trainingResults
-from .checkpoints import  checkpoint, printLoss, earlyStopping
--- GN4IP/old/models/__init__.py
+++
@@ -1,3 +0,0 @@
-# GN4IP/models __init__ file
-
-from .build import buildModel, parameterSummary
